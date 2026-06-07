b8d7b3cf9d7564b3b57d12a33d7ebbd858bbb0c2
BozidarRakonjac <rakonjacbozidar9@gmail.com>
2026-06-07 22:04:39 +0200

    feat: adding telemetry_loader

--- simulator/main.py
+++ simulator/main.py
@@ -2,10 +2,12 @@
 
 from db.connection import get_connection
 from db.create_tables import create_tables
+from loaders.telemetry_loader import load_telemetry
 
 def main():
     conn = get_connection()
     create_tables(conn)
+    load_telemetry(conn)
     conn.close()
     print("Done!")
 
