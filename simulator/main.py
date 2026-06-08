efe27dbe4a4a13030deb847f43d41b25fb3100c1
BozidarRakonjac <rakonjacbozidar9@gmail.com>
2026-06-08 12:07:29 +0200

    feat: inserted data in quality_inspections

--- simulator/main.py
+++ simulator/main.py
@@ -4,12 +4,14 @@ from db.connection import get_connection
 from db.create_tables import create_tables
 from loaders.telemetry_loader import load_telemetry
 from loaders.downtime_loader import load_downtime
+from loaders.quality_loader import load_quality
 
 def main():
     conn = get_connection()
     create_tables(conn)
     load_telemetry(conn)
     load_downtime(conn)
+    load_quality(conn)
     conn.close()
     print("Done!")
 
