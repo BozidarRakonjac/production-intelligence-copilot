5013f706e984a80eb029689ff2f91709147519d0
BozidarRakonjac <rakonjacbozidar9@gmail.com>
2026-06-12 14:00:13 +0200

    feat: added silver in definitions

--- dagster/definitions.py
+++ dagster/definitions.py
@@ -1,17 +1,25 @@
 from dagster import Definitions
 from resources.postgres import PostgresResource
 import os
-from assets.bronze.machine_telemetry import bronze_machine_telemetry
-from assets.bronze.downtime_logs import bronze_downtime_logs
-from assets.bronze.quality_inspections import bronze_quality_inspections
-from assets.bronze.production_events import bronze_production_events
+from dagster.assets.bronze.bronze_machine_telemetry import bronze_machine_telemetry
+from dagster.assets.bronze.bronze_downtime_logs import bronze_downtime_logs
+from dagster.assets.bronze.bronze_quality_inspections import bronze_quality_inspections
+from dagster.assets.bronze.bronze_production_events import bronze_production_events
+from assets.silver.silver_machine_telemetry import silver_machine_telemetry
+from assets.silver.silver_downtime_logs import silver_downtime_logs
+from assets.silver.silver_quality_inspections import silver_quality_inspections
+from assets.silver.silver_production_events import silver_production_events
 
 defs = Definitions(
     assets=[
         bronze_machine_telemetry,
         bronze_downtime_logs,
         bronze_quality_inspections,
-        bronze_production_events
+        bronze_production_events,
+        silver_machine_telemetry,
+        silver_downtime_logs,
+        silver_quality_inspections,
+        silver_production_events
     ],
     resources={
         "postgres": PostgresResource(
